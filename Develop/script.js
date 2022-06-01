ed67c8aa328f295ef524e17fd0dff98e63a3d8eb
Matt Larkin <mlarkin14@gmail.com>
2022-06-01 10:28:28 -0400

    refactored code and added comments

--- Develop/script.js
+++ Develop/script.js
@@ -1,82 +1,82 @@
 // Assignment code here
 // Password digits arrays
-var number = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-var specialChar = ["!", "%", "&", ",", "*", "+", "-", ".", "/", "<", ">", "?", "~"];
-var alphaLower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-var alphaUpper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-
-// Input variables 
-var pwLength = 0;
-
-
-var passwordCharacters = [];
-console.log(generateBtn);
+const number = "0123456789";
+const specialChar = "!#$%&'()*+,-./:;<=>?@[]^_`{|}~";
+const alphaLower = "abcdefghijklmnopqrstuvwxyz";
+const alphaUpper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
 function generatePassword() {
-    SetPasswordLength();
-    // console.log("pwLength: " + pwLength);  
-    GetPasswordRequirements();
-    // console.log (passwordCharacters);
-    return BuildPassword();
 
-    function SetPasswordLength() {
-        getPasswordLength();
-        // while getPasswordLength() < 8 || getPasswordLength() > 128 keep getting length.
-        while (pwLength < 8 || pwLength > 128) {
+    // Build Password using random characters
+    var newUserPassword = function(n, str) {
+        var userPassword = "";
+        for (var i = 0; i < n; i++) {
+            userPassword += str[Math.floor(Math.random() * str.length)];
+        }
+        return userPassword;
+    };
+
+    return newUserPassword(SetPasswordLength(), BuildPasswordCharacterArray());
+}
+
+// Get references to the #generate element
+var generateBtn = document.querySelector("#generate");
+console.log(generateBtn);
+
+// Write password to the #password input
+function writePassword() {
+    var password = generatePassword();
+    var passwordText = document.querySelector("#password");
+
+    passwordText.value = password;
+
+}
+
+// Set Password Length and Test values
+function SetPasswordLength() {
+    var pwLength = 0;
+    var valid = false;
+
+    while (!valid) {
+        pwLength = parseInt(window.prompt("Enter Password Lenght (8-128 characters)."));
+
+        if (pwLength >= 8 && pwLength <= 128) {
+            valid = true;
+            return pwLength;
+        } else {
             window.alert("Password must be more than 8 characters and less than 128 characters.");
-            getPasswordLength();
         }
     }
-    // Write password to the #password input
-    function writePassword() {
-        var password = generatePassword();
-        var passwordText = document.querySelector("#password");
 
-        passwordText.value = password;
+}
 
-    }
-
-    function getPasswordLength() {
-        pwLength = parseInt(window.prompt("Enter Password Lenght (8-128 characters)."));
-    }
-
-    function BuildPasswordCharacterArray() {
+// Build the avalible characters array
+function BuildPasswordCharacterArray() {
+    var passwordCharacters = "";
+    while (passwordCharacters.length < 1) {
         var confirmSpecialCharacter = confirm("Click OK to confirm if you would like to include special characters");
         var confirmNumericCharacter = confirm("Click OK to confirm if you would like to include numeric characters");
         var confirmLowerCase = confirm("Click OK to confirm if you would like to include lowercase characters");
         var confirmUpperCase = confirm("Click OK to confirm if you would like to include uppercase characters");
 
+        if (confirmSpecialCharacter) {
+            passwordCharacters = passwordCharacters.concat(specialChar);
+        }
+        if (confirmLowerCase) {
+            passwordCharacters = passwordCharacters.concat(alphaLower);
+        }
+        if (confirmUpperCase) {
+            passwordCharacters = passwordCharacters.concat(alphaUpper);
+        }
+        if (confirmNumericCharacter) {
+            passwordCharacters = passwordCharacters.concat(number);
+        }
+        if (passwordCharacters.length == 0) {
+            alert("You must select at least one character type!");
+        }
     }
-
-    // Add event listener to generate button
-    generateBtn.addEventListener("click", writePassword);
-
-    if (confirmSpecialCharacter) {
-        passwordCharacters = passwordCharacters.concat(specialChar);
-    }
-    if (confirmLowerCase) {
-        passwordCharacters = passwordCharacters.concat(alphaLower);
-    }
-    if (confirmUpperCase) {
-        passwordCharacters = passwordCharacters.concat(alphaUpper);
-    }
-    if (confirmNumericCharacter) {
-        passwordCharacters = passwordCharacters.concat(number);
-    }
+    return passwordCharacters;
 }
 
-function GetPasswordRequirements() {
-    BuildPasswordCharacterArray();
-    while (passwordCharacters.length == 0) {
-        window.alert("You must select at least one character type!");
-        BuildPasswordCharacterArray();
-    }
-}
-
-function BuildPassword() {
-    var userPassword = "";
-    for (var i = 0; i < pwLength; i++) {
-        userPassword += passwordCharacters[Math.floor(Math.random() * passwordCharacters.length)];
-    }
-    // console.log(userPassword);
-    return userPassword;
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
