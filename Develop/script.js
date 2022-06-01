811a1f595ad98449178137c4b593a4b21dbbbf80
Matt Larkin <mlarkin14@gmail.com>
2022-06-01 10:25:31 -0400

    added password generate function and listeners

--- Develop/script.js
+++ Develop/script.js
@@ -7,49 +7,76 @@ var alphaUpper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M
 
 // Input variables 
 var pwLength = 0;
-var confirmSpecialCharacter;
-var confirmNumericCharacter;
-var confirmUpperCase;
-var confirmLowerCase;
 
 
-function generatePassword() {
-    getPasswordLength();
-    // while getPasswordLength() < 8 || getPasswordLength() > 128 keep getting length.
-    while (pwLength < 8 || pwLength > 128) {
-        window.alert("Password must be more than 8 characters and less than 128 characters.");
-        getPasswordLength();
-    }
-    console.log("pwLength: " + pwLength);
-
-    var passwordCharacters = BuildPasswordCharacterArray();
-
-}
-
-// Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
+var passwordCharacters = [];
 console.log(generateBtn);
 
-// Write password to the #password input
-function writePassword() {
-    var password = generatePassword();
-    var passwordText = document.querySelector("#password");
+function generatePassword() {
+    SetPasswordLength();
+    // console.log("pwLength: " + pwLength);  
+    GetPasswordRequirements();
+    // console.log (passwordCharacters);
+    return BuildPassword();
 
-    passwordText.value = password;
+    function SetPasswordLength() {
+        getPasswordLength();
+        // while getPasswordLength() < 8 || getPasswordLength() > 128 keep getting length.
+        while (pwLength < 8 || pwLength > 128) {
+            window.alert("Password must be more than 8 characters and less than 128 characters.");
+            getPasswordLength();
+        }
+    }
+    // Write password to the #password input
+    function writePassword() {
+        var password = generatePassword();
+        var passwordText = document.querySelector("#password");
 
+        passwordText.value = password;
+
+    }
+
+    function getPasswordLength() {
+        pwLength = parseInt(window.prompt("Enter Password Lenght (8-128 characters)."));
+    }
+
+    function BuildPasswordCharacterArray() {
+        var confirmSpecialCharacter = confirm("Click OK to confirm if you would like to include special characters");
+        var confirmNumericCharacter = confirm("Click OK to confirm if you would like to include numeric characters");
+        var confirmLowerCase = confirm("Click OK to confirm if you would like to include lowercase characters");
+        var confirmUpperCase = confirm("Click OK to confirm if you would like to include uppercase characters");
+
+    }
+
+    // Add event listener to generate button
+    generateBtn.addEventListener("click", writePassword);
+
+    if (confirmSpecialCharacter) {
+        passwordCharacters = passwordCharacters.concat(specialChar);
+    }
+    if (confirmLowerCase) {
+        passwordCharacters = passwordCharacters.concat(alphaLower);
+    }
+    if (confirmUpperCase) {
+        passwordCharacters = passwordCharacters.concat(alphaUpper);
+    }
+    if (confirmNumericCharacter) {
+        passwordCharacters = passwordCharacters.concat(number);
+    }
 }
 
-function getPasswordLength() {
-    pwLength = parseInt(window.prompt("Enter Password Lenght (8-128 characters)."));
+function GetPasswordRequirements() {
+    BuildPasswordCharacterArray();
+    while (passwordCharacters.length == 0) {
+        window.alert("You must select at least one character type!");
+        BuildPasswordCharacterArray();
+    }
 }
 
-function BuildPasswordCharacterArray() {
-    var confirmSpecialCharacter = confirm("Click OK to confirm if you would like to include special characters");
-    var confirmNumericCharacter = confirm("Click OK to confirm if you would like to include numeric characters");
-    var confirmLowerCase = confirm("Click OK to confirm if you would like to include lowercase characters");
-    var confirmUpperCase = confirm("Click OK to confirm if you would like to include uppercase characters");
-
-}
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+function BuildPassword() {
+    var userPassword = "";
+    for (var i = 0; i < pwLength; i++) {
+        userPassword += passwordCharacters[Math.floor(Math.random() * passwordCharacters.length)];
+    }
+    // console.log(userPassword);
+    return userPassword;
